cfce51b880afeded1f1bf21225981d1ebbd71043
David <dmesser@pm.me>
2022-07-30 17:34:18 -0400

    Test deployment

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,57 @@
+import Head from "next/head";
+import {MongoClient} from "mongodb";
+
 import MeetupList from "../components/meetups/MeetupList";
-import Layout from "../components/layout/Layout";
 
-const DUMMY_MEETUPS = [
-    {
-        id: 'm1',
-        title: 'A First Meetup',
-        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/800px-Stadtbild_M%C3%BCnchen.jpg',
-        address: 'Some address 5, 12345 Some City',
-        description: 'This is a first meetup!'
-    },
-    {
-        id: 'm2',
-        title: 'A Second Meetup',
-        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/800px-Stadtbild_M%C3%BCnchen.jpg',
-        address: 'Some address 5, 12345 Some City',
-        description: 'This is a second meetup!'
-    },
-    {
-        id: 'm3',
-        title: 'A Third Meetup',
-        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/800px-Stadtbild_M%C3%BCnchen.jpg',
-        address: 'Some address 5, 12345 Some City',
-        description: 'This is a third meetup!'
-    }
-]
-
-function HomePage() {
+function HomePage(props) {
     return (
-            <MeetupList meetups={DUMMY_MEETUPS} />
+        <>
+            <Head>
+                <title>React Meetups</title>
+                <meta name='description' content='Browse a huge list of highly active React meetups!'/>
+            </Head>
+            <MeetupList meetups={props.meetups} />
+
+        </>
     )
+}
+
+
+
+// export async function getServerSideProps (context) {
+//     const req = context.req;
+//     const res = context.res;
+//
+//     return {
+//         props: {
+//             meetups: DUMMY_MEETUPS
+//         }
+//     }
+// }
+
+
+export async function getStaticProps() {
+    const client = await MongoClient.connect('mongodb+srv://david:J4UESw2EEVbL7ZXP@cluster0.lgrluan.mongodb.net/meetups?retryWrites=true&w=majority');
+
+    const db = client.db();
+
+    const meetupsCollection = db.collection('meetups');
+
+    const meetups = await meetupsCollection.find().toArray();
+
+    client.close();
+
+    return {
+        props: {
+            meetups: meetups.map(meetup => ({
+                title: meetup.title,
+                address: meetup.address,
+                image: meetup.image,
+                id: meetup._id.toString()
+            }))
+        },
+        revalidate: 1
+    };
 }
 
 export default HomePage;
--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,13 +1,36 @@
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+import {useRouter} from "next/router";
+import Head from "next/head";
 
-function NewMeetup() {
-    function onMeetupHandler(enteredMeetupData) {
-        console.log(enteredMeetupData);
+function NewMeetupPage() {
+    const router = useRouter();
+
+    async function addMeetupHandler(enteredMeetups) {
+        const response = await fetch('/api/new-meetup', {
+            method: 'POST',
+            body: JSON.stringify(enteredMeetups),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        const data = await response.json();
+
+        console.log(data);
+
+        router.push('/');
     }
 
     return (
-        <NewMeetupForm onAddMeetup={onMeetupHandler} />
+        <>
+            <Head>
+                <title>Add a New Meetup</title>
+                <meta name='description' content='Add your own meetups and create amazing networking opportunities.' />
+            </Head>
+            <NewMeetupForm onAddMeetup={addMeetupHandler}/>
+        </>
+
     )
 }
 
-export default NewMeetup;
+export default NewMeetupPage;
